57cc75ced51920485ce402f18fc830271ea14b5c
Deni Malik irawan <43429941+irawandeni@users.noreply.github.com>
2023-06-15 11:51:54 +0700

    app

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 5.0
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?>. Sekolah Dasar</strong> SD Negeri Jatimulyo
+</footer>
